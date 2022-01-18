7a72466e1c0c6a5b827f68150b0c0cb324d07245
Théo Guillerminet <theo.guillerminet@ecomail.io>
2022-01-18 16:58:50 +0100

    new file:   scripts/download_from_google_drive.sh

---
+++ scripts/download_from_google_drive.sh
@@ -0,0 +1,15 @@
+#METADATA
+#gdown 'https://drive.google.com/uc?export=download&id=1qOzys6wmWXGDDwgGs46XF4GGNarq3_JG'
+#mv metadata_saccharomyces_cerevisiae.xlsx ~/winhome/Desktop/Projets/sourdough_evolution/data/metadata_saccharomyces_cerevisiae.xlsx
+
+#STRAINS
+gdown 'https://drive.google.com/uc?export=download&id=1KCI63qEKIZtpADd9bRkHEi_vC1766TW4'
+mv strains.xlsx ~/winhome/Desktop/Projets/sourdough_evolution/data/strains.xlsx
+
+#BAKERS
+gdown 'https://drive.google.com/uc?export=download&id=1Uq5o-wpCG_IcTDcxoE6sblQJCEbLKahT'
+mv bakers.xlsx ~/winhome/Desktop/Projets/sourdough_evolution/data/bakers.xlsx
+
+#FLOURS
+gdown 'https://drive.google.com/uc?export=download&id=1EfZ-MjVZnXt2hdmegpVIOdhY5Ywz_VGI'
+mv flours.xlsx ~/winhome/Desktop/Projets/sourdough_evolution/data/flours.xlsx
