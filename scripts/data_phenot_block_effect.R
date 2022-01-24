96734a1ba219ecf2d76a55347e9d7b7e291b6570
Théo Guillerminet <theo.guillerminet@ecomail.io>
2022-01-24 16:48:31 +0100

    new file:   scripts/data_phenot_block_effect.R 	deleted:    scripts/download_from_google_drive.sh 	Figure_rapport_debloc.pdf 	RapportSourdoughOther_debloc.csv 	myDataCorr.csv 	scripts/script_domestication_JL_v3.Rmd 	scripts/script_domestication_JL_v3.html 	scripts/script_domestication_JL_v3.tex

---
+++ scripts/data_phenot_block_effect.R
@@ -0,0 +1,87 @@
+
+# Block effect ####
+
+traits <- c("cell_t27", "co2max", "death_prct", "lag", "tvmax", "vmax")
+
+data_phenot_parms_clean %>%
+  group_by(parameter) %>%
+  summarise()
+  
+
+## 1. Estimation des effets blocs avec un modele par milieu
+# On estime l'effet bloc uniquement avec le Sourdough.
+
+LMERS0 = lmer(logtrait ~ Species + (1|Strain) +(1|Block), data=mydataS,REML=TRUE)
+
+
+## 2. Modèle complet
+# On travaille sur l'echelle log (résidus mieux pour certaines variables + 
+# interprétation est la même pour tous : on estime un rapport). On estime 
+# l'effet bloc pour le comparer à celui qu'on a estimé en 1.
+
+LMER0 = lmer(logtrait ~Species * Habitat+(1|Strain)+(1|Block),data=mydataV,REML=TRUE)
+
+
+## 3. Estimation de l'effet bloc en effet fixe
+# Avec seulement les souches présentes dans les deux blocs
+# On teste l'interaction, puis on estime l'effet dans le modèle additif si pas d'interaction
+#on travaille sur la variable en log et on calcul des effets fixes Souche et block
+
+LM0= lm(logtrait ~ Strain*Block,data=mydataV)
+print(anova(LM0))
+
+AnoInter<-rbind.data.frame(AnoInter,cbind.data.frame(trait=trait,pInter=anova(LM0)$P[3]))
+
+LM0= lm(logtrait ~ Strain+Block,data=mydataV)
+
+
+## 3'. Modèle complet sur echelle log
+# on retire au préalable l'effet bloc
+# On estime l'effet milieu pour chaque espèce.
+# On stocke estimation et IC
+
+
+
+
+strains_distrib_inblocs <- data_cyto %>%
+  select(strain_name, bloc) %>%
+  unique() %>%
+  group_by(strain_name) %>%
+  summarise(nb_blocs = n()) %>%
+  arrange(nb_blocs)
+
+data_phenot_parms_clean %>%
+  mutate(parameter = add_units(parameter)) %>%
+  ggplot(.) +
+  aes(y = value, fill = bloc) +
+  geom_boxplot(alpha = 0.3) +
+  facet_wrap(~parameter, scales = "free") +
+  theme_minimal()
+
+bloc_effect_model <- data_phenot_parms_clean %>%
+  group_by(parameter) %>%
+  summarise(p.value = anova(lm(value ~ bloc))$'Pr(>F)'[1])
+
+
+
+
+
+## Data structure : distribution of strains among blocks ####
+links <- table(data_cyto$strain_name, data_cyto$bloc) %>%
+  as.data.frame() %>%
+  filter(Freq != 0)
+
+nodes <- data.frame(name=c(as.character(links$Var1),
+                           as.character(links$Var2)
+) %>% unique()
+)
+
+links$IDsource <- match(links$Var1, nodes$name)-1 
+links$IDtarget <- match(links$Var2, nodes$name)-1
+
+p <- networkD3::sankeyNetwork(Links = links, Nodes = nodes,
+                              Source = "IDsource", Target = "IDtarget",
+                              Value = "Freq", NodeID = "name", 
+                              sinksRight=FALSE, iterations = 100,
+                              width = 500, height = 1500)
+htmlwidgets::saveWidget(p, file=paste0(getwd(), "/output/sankeyBlocks.html"))
